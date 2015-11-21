8f7649748b18e284654f3baab889bb55bb5f846e
Fabian Schmitthenner <development@schmitthenner.eu>
2015-11-21 21:13:39 +0000

    change sdb5 to more robust uuid

--- original/mount.nix
+++ original/mount.nix
@@ -4,7 +4,7 @@
   environment.systemPackages = [
     (pkgs.writeScriptBin "mountFilesystem" ''
       #!${pkgs.bash}/bin/bash
-      ${pkgs.cryptsetup}/bin/cryptsetup luksOpen /dev/sdb5 OldEncrypted
+      ${pkgs.cryptsetup}/bin/cryptsetup luksOpen /dev/disk/by-uuid/a6fd70c7-5e8e-42bf-80b4-ee90dd52f93c OldEncrypted
       ${pkgs.devicemapper}/bin/lvm vgchange -ay main
       ${pkgs.eject}/bin/mount /dev/main/DebianTesting /unpure
       '')
