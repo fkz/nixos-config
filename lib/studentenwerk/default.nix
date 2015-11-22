5af6e568666b7c44381888dc3be56fb3988a5f32
Fabian Schmitthenner <development@schmitthenner.eu>
2015-11-22 01:42:06 +0000

    add possibility to build without passwords

--- lib/studentenwerk/default.nix
+++ lib/studentenwerk/default.nix
@@ -1,22 +1,32 @@
-{ pkgs, ... }:
+{ pkgs, lib, config, ... }:
 {
-  services.openvpn.servers.studentenwerkBonn = { 
-      config = ''
-        remote vpn.stw-bonn.de 1194 udp
-        dev tun0
-        persist-tun
-        persist-key
-        ca ${./ca--bonn.crt}
-        remote-random
-        resolv-retry infinite
-        tls-remote vpn.stw-bonn.de
-        auth-user-pass ${./openvpn-auth}
-        #auth-user-pass
-        client
-        nobind
-        pull
-        verb 4
-        redirect-gateway def1
-        '';
+  options = {
+    services.studentenwerkBonn.enable = lib.mkOption {
+      default = true;
+      description = ''
+        enable if you want to connect to the vpn
+      '';
+    };
+  };
+  config = lib.mkIf (config.services.studentenwerkBonn.enable) {
+    services.openvpn.servers.studentenwerkBonn = { 
+        config = ''
+          remote vpn.stw-bonn.de 1194 udp
+          dev tun0
+          persist-tun
+          persist-key
+          ca ${./ca--bonn.crt}
+          remote-random
+          resolv-retry infinite
+          tls-remote vpn.stw-bonn.de
+          auth-user-pass ${./openvpn-auth}
+          #auth-user-pass
+          client
+          nobind
+          pull
+          verb 4
+          redirect-gateway def1
+          '';
+      };
     };
 }
--- original/mount.nix
+++ original/mount.nix
@@ -5,8 +5,9 @@
     (pkgs.writeScriptBin "mountFilesystem" ''
       #!${pkgs.bash}/bin/bash
       if [ $1 eq '-r' ]; then
-        ${
-      done
+        echo "Not implemented yet"
+        exit 1
+      fi
       ${pkgs.cryptsetup}/bin/cryptsetup luksOpen /dev/disk/by-uuid/a6fd70c7-5e8e-42bf-80b4-ee90dd52f93c OldEncrypted
       ${pkgs.devicemapper}/bin/lvm vgchange -ay main
       ${pkgs.eject}/bin/mount /dev/main/DebianTesting /unpure
