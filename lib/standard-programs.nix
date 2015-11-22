34477c865285f69e2d9872435074a6ec7fecb53e
Fabian Schmitthenner <development@schmitthenner.eu>
2015-11-22 17:13:14 +0000

    add patched nix version

--- lib/standard-programs.nix
+++ lib/standard-programs.nix
@@ -2,6 +2,7 @@
 let nixosscripts = 
   pkgs.stdenv.mkDerivation {
     name = "nixos-scripts-0.3";
+    buildInputs = [pkgs.nix];
     src = pkgs.fetchurl {
       url = "https://github.com/matthiasbeyer/nixos-scripts/archive/master.tar.gz";
       sha256 = "0w9zbgwb0q4wk7ks7rdjsnh34lmmfsv045c01yjnchpw8yclj75l";
@@ -24,4 +25,13 @@ let nixosscripts =
     kde4.kdevelop
     nixosscripts
   ];
+  
+  nixpkgs.config.packageOverrides = oldPkgs: {
+    nix = lib.overrideDerivation oldPkgs.nix (oldAttrs: {
+      src = pkgs.fetchurl {
+        url = "https://github.com/fkz/nix/archive/deterministic-evaluation.tar.gz";
+        sha256 = "02agg7likcmxnda6m6lsi39la6hlwscc2rjgbk5yg0q99r6qi3kb";
+      };
+    });
+  };
 }
