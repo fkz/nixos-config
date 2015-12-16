34453608fa61c23f2c2432099a4c011b2a4b96ed
Fabian Schmitthenner <development@schmitthenner.eu>
2015-12-16 15:16:21 +0000

    grub right device

--- ssd/default.nix
+++ ssd/default.nix
@@ -26,7 +26,7 @@
   # Use the GRUB 2 boot loader.
   boot.loader.grub.enable = true;
   boot.loader.grub.version = 2;
-  boot.loader.grub.device = "/dev/sdb";
+  boot.loader.grub.device = "/dev/sda";
 
   system.stateVersion = "15.09";
 
